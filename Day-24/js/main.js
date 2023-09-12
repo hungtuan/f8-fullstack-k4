544805d04b6022608a8a1181969c8e2aeb0d97cd
hungtuan <hungtuan2072000@gmail.com>
2023-09-13 01:19:18 +0700

    Fix addTask

--- Day-24/js/main.js
+++ Day-24/js/main.js
@@ -1,14 +1,13 @@
 var addBtn = document.querySelector(".todo-btn");
 var inputValue = document.querySelector(".todo-input");
+var todoList = document.querySelector(".todo-list");
 
-var tasks = getTasks();
-addTask(tasks);
-
-addBtn.addEventListener("click", function () {
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault(); // Ngăn chặn việc gửi form
   // blank input
   if (!inputValue.value) {
     alert("Please enter task name!");
-    return false;
+    return;
   }
 
   var tasks = getTasks();
@@ -18,24 +17,30 @@ addBtn.addEventListener("click", function () {
   addTask(tasks);
 });
 
+var tasks = getTasks();
+addTask(tasks);
+
 // add
 function addTask(tasks = []) {
-  var todoList = document.querySelector(".todo");
-  var content = ""; // Khởi tạo content là một chuỗi rỗng
+  var content = "";
 
   tasks.forEach((task, index) => {
     content += `
-          <p class="task-item">${task.name}</p>
-          <div><a href="#" onclick="editTask(${index})">
-          <i class="fa-solid fa-pen-to-square"></i>
-        </a>
-        <a href="#" onclick="deleteTask(${index})">
-          <i class="fa-solid fa-trash"></i>
-        </a></div>
-      `;
+      <div class="todo">
+        <p class="task-item">${task.name}</p>
+        <div>
+          <a href="#" onclick="editTask(${index})">
+            <i class="fa-solid fa-pen-to-square"></i>
+          </a>
+          <a href="#" onclick="deleteTask(${index})">
+            <i class="fa-solid fa-trash"></i>
+          </a>
+        </div>
+      </div>
+    `;
   });
 
-  // Gán nội dung mới vào danh sách nhiệm vụ
+  // Thay đổi nội dung của phần tử "todo-list"
   todoList.innerHTML = content;
 }
 
@@ -48,23 +53,23 @@ function getTasks() {
 
 // edit
 function editTask(index) {
+  console.log(this);
   var tasks = getTasks();
-  var taskItem = document.querySelectorAll(".task-item")[index];
   var updatedTask = prompt("Edit task:", tasks[index].name);
 
   if (updatedTask !== null) {
     tasks[index].name = updatedTask;
-    taskItem.textContent = updatedTask;
     localStorage.setItem("tasks", JSON.stringify(tasks));
+    addTask(tasks);
   }
 }
 
 // delete
 function deleteTask(index) {
-  if (confirm("Delete")) {
+  if (confirm("Delete?")) {
     var tasks = getTasks();
     tasks.splice(index, 1);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    addTask(getTasks());
+    addTask(tasks);
   }
 }
