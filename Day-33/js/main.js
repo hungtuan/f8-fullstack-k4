6b3593663bfc0ac3791b0c38063dda41312581e1
hungtuan <hungtuan2072000@gmail.com>
2023-10-15 00:59:59 +0700

    update code

--- Day-33/js/main.js
+++ Day-33/js/main.js
@@ -3,84 +3,92 @@ const output = document.querySelector(".output");
 const action = document.querySelector(".action");
 
 function speechToText() {
-  const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
+  var transcript = null;
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
   recognition.continuous = false;
   recognition.interimResults = true;
   recognition.lang = "vi-VN";
+  recognition.start();
 
-  button.style.background = "orange";
-  action.innerHTML = "Đang nghe, vui lòng nói yêu cầu của bạn...";
+  recognition.onstart = function () {
+    action.innerHTML = "Vui lòng nói yêu cầu của bạn...";
+    button.style.background = "orange";
+  };
 
-  recognition.onend = function () {
-    button.style.background = "#549c24";
-    action.innerHTML = "Dừng nghe, hi vọng kết quả theo ý bạn";
+  recognition.onspeechend = function () {
+    action.innerHTML = "Nhấp nút trên để nói";
+    button.style.background = "#3498db";
+    recognition.stop();
   };
 
   recognition.onresult = function (event) {
-    const transcript = event.results[0][0].transcript.toLowerCase();
+    transcript = event.results[0][0].transcript;
     output.innerHTML = "<b>Kết quả: </b>" + transcript;
-    processTranscript(transcript);
   };
+  recognition.onend = function () {
+    if (transcript) {
+      transcript = transcript.toLowerCase().replace(/[.,?]/g, "");
 
-  recognition.start();
-}
+      switch (transcript) {
+        case "youtube":
+          window.open("https://www.youtube.com");
+          break;
+        case "facebook":
+          window.open("https://www.facebook.com");
+          break;
+        case "google":
+          window.open("https://www.google.com");
+          break;
+        case "instagram":
+          window.open("https://www.instagram.com");
+          break;
+        case "google drive":
+          window.open("https://drive.google.com");
+          break;
+        case "google maps":
+          window.open("https://maps.google.com");
+          break;
+        case "bản đồ":
+          window.open("https://maps.google.com");
+          break;
+        default:
+          const commandMappings = [
+            {
+              keywords: ["chỉ đường", "chỉ đường tới", "đường tới", "tới"],
+              url: "https://www.google.com/maps/place/",
+            },
+            {
+              keywords: ["bài hát", "mở bài hát", "nghe bài hát"],
+              url: "https://zingmp3.vn/tim-kiem/tat-ca?q=",
+            },
+            {
+              keywords: ["video", "mở video", "xem video"],
+              url: "https://www.youtube.com/results?search_query=",
+            },
+          ];
 
-function processTranscript(transcript) {
-  const openWebsite = (url) => window.open(url, "_blank");
-  transcript = transcript.toLowerCase().replace(/[.,?]/g, "");
-  switch (transcript) {
-    case "youtube":
-      openWebsite("https://www.youtube.com");
-      break;
-    case "facebook":
-      openWebsite("https://www.facebook.com");
-      break;
-    case "google":
-      openWebsite("https://www.google.com");
-      break;
-    case "instagram":
-      openWebsite("https://www.instagram.com");
-      break;
-    case "google drive":
-      openWebsite("https://drive.google.com");
-      break;
-    case "google maps":
-      openWebsite("https://maps.google.com");
-      break;
-    case "bản đồ":
-      openWebsite("https://maps.google.com");
-      break;
-    default:
-      if (transcript.startsWith("chỉ đường") || transcript.startsWith("tới")) {
-        const location = transcript
-          .replace("chỉ đường", "")
-          .replace("tới", "")
-          .trim();
-        openWebsite(`https://www.google.com/maps/place/${location}/`);
-      } else if (
-        transcript.startsWith("mở bài hát") ||
-        transcript.startsWith("nghe bài hát")
-      ) {
-        const song = transcript
-          .replace("mở bài hát", "")
-          .replace("nghe bài hát", "")
-          .trim();
-        openWebsite(`https://zingmp3.vn/tim-kiem/tat-ca?q=${song}`);
-      } else if (
-        transcript.startsWith("mở video") ||
-        transcript.startsWith("xem video")
-      ) {
-        const video = transcript
-          .replace("mở video", "")
-          .replace("xem video", "")
-          .trim();
-        openWebsite(`https://www.youtube.com/results?search_query=${video}`);
-      } else {
-        action.innerHTML = "<b>Kết quả: Không thực hiện được yêu cầu</b>";
+          let matchedCommand = null;
+
+          for (const mapping of commandMappings) {
+            for (const keyword of mapping.keywords) {
+              if (transcript.includes(keyword)) {
+                matchedCommand = mapping;
+                transcript = transcript.replace(new RegExp(keyword, "g"), "");
+                break;
+              }
+            }
+            if (matchedCommand) break;
+          }
+
+          if (matchedCommand) {
+            window.open(`${matchedCommand.url}${transcript}`);
+          } else {
+            action.innerHTML = "<b>Không thực hiện được yêu cầu</b>";
+          }
       }
-  }
+    }
+  };
 }
 
 button.addEventListener("click", speechToText);
